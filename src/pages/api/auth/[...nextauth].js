94131f260cbf1c82811975385c6022a303d1f18b
Yusuf Shaikh <yusufshaikh05824@gmail.com>
2023-10-03 15:34:53 +0530

    update on auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      secret: process.env.NEXTAUTH_SECRET
     }),
     // ...add more providers here
   ],
